4e4409cfe1d2a9d5fa84d3df6bbd7fe31a6eb4c0
Travis Halleck <hallectj@gmail.com>
2023-08-02 20:57:24 -0500

    Added sidebar card
    Will make dynamic on next checkin

--- src/app/common/home/home.component.ts
+++ src/app/common/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomeService } from './home.service';
 
 @Component({
   selector: 'home',
@@ -6,13 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  public randomYear: string = "1990";
   
-  constructor(){
-
-  }
+  constructor(public service: HomeService){}
 
   public ngOnInit(){
+    this.randomYear = this.getRandomYear(1940, 2020).toString();
+  }
 
+  //years from 1940 to 2020
+  private getRandomYear(min: number, max: number): number {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
   }
 }
--- src/app/modules/info-sidebar/info-sidebar.component.ts
+++ src/app/modules/info-sidebar/info-sidebar.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HomeService } from 'src/app/common/home/home.service';
 
 @Component({
-  selector: 'app-info-sidebar',
+  selector: 'info-sidebar',
   templateUrl: './info-sidebar.component.html',
   styleUrls: ['./info-sidebar.component.scss']
 })
-export class InfoSidebarComponent {
 
+export class InfoSidebarComponent {
+  public youtubeURL: string = "https://www.youtube.com/watch?v=3KFvoDDs0XM";
+  public thumbnail: string = "";
+  public youtubeApi: ReturnType<InfoSidebarComponent['youtube']> | undefined;
+  
+  @Input() randomYear: string = "1990";
+  
+  constructor(public homeService: HomeService){}
+
+  public youtube = () => {
+    let video: string = "", results: RegExpMatchArray | null;
+  
+    const youtubeObj = {
+      thumbnail: (url: string) => {
+        if (url === null) { return ""; }
+        results = url.match('[\\?&]v=([^&#]*)');
+        video = (results == null) ? url : results[1];
+        return `http://img.youtube.com/vi/${video}/2.jpg`;
+      },
+    };
+  
+    return youtubeObj;
+  };
+
+  ngOnInit(){
+    this.youtubeApi = this.youtube();
+    this.thumbnail = this.youtubeApi.thumbnail(this.youtubeURL);
+  }
 }
