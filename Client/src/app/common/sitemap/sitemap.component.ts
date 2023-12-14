8dcc695c5ab68ba70299474c20befe8c3351ce50
Travis Halleck <hallectj@gmail.com>
2023-12-13 23:46:57 -0600

    injecting document in sitemap component

--- Client/src/app/common/sitemap/sitemap.component.ts
+++ Client/src/app/common/sitemap/sitemap.component.ts
@@ -1,5 +1,6 @@
+import { DOCUMENT } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-sitemap',
@@ -9,12 +10,12 @@ export class SitemapComponent {
   public server_url = 'https://funlifeapp-011573cdfdc6.herokuapp.com';
   //public server_url = 'http://localhost:5000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
     const headers = new HttpHeaders({ 'Content-Type': 'text/xml' }).set('Accept', 'text/xml')
     this.http.get(this.server_url + '/sitemap.xml', { headers: headers, responseType: 'text' }).subscribe((data) => {
-      document.write(data)
+      this.document.write(data)
     })
   }
 }
