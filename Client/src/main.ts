3410e028810447cd08aa8aad0f764bac638e0ddb
Travis Halleck <hallectj@gmail.com>
2025-02-25 23:12:27 -0600

    vercel analytics

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { inject } from '@vercel/analytics';
 import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => inject())
   .catch(err => console.error(err));
