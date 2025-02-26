84362cac1c086b196b0fe9e0bbb4e5955476e499
Travis Halleck <hallectj@gmail.com>
2025-02-26 04:18:05 -0600

    reverting back to old SSR changes

--- Client/src/app/modules/hot-hundred-main/hot-hundred-year/hot-hundred-year.component.ts
+++ Client/src/app/modules/hot-hundred-main/hot-hundred-year/hot-hundred-year.component.ts
@@ -4,7 +4,6 @@ import { slugify } from '../../../../app/common/Toolbox/util';
 import { ISong2 } from '../../../../app/models/shared-models';
 import { GeneralService } from '../../../../app/services/general.service';
 import { Meta, Title } from '@angular/platform-browser';
-import { first } from 'rxjs';
 
 @Component({
   selector: 'app-hot-hundred-year',
@@ -24,22 +23,24 @@ export class HotHundredYearComponent {
   ){}
 
   public ngOnInit(): void {
-    this.route.params.pipe(first()).subscribe((params) => {
+    this.route.params.subscribe((params) => {
       this.selectedYear = params['year'];
       if (this.selectedYear) {
         const yearNum = +this.selectedYear;
+
+        // Update title and meta tags (runs on both server and client)
         this.title.setTitle(`Hot Hundred Songs of ${this.selectedYear}`);
         this.meta.updateTag({ name: 'description', content: `Hot Hundred Songs of ${this.selectedYear}` });
+
+        // Notify child components (assuming this is synchronous or handled elsewhere)
         this.service.sendYearToChild(yearNum);
 
-        // Fetch synchronously for SSR
-        this.service.getSongs(yearNum, 'position').pipe(first()).subscribe({
-          next: (songs) => {
-            console.log(`[${yearNum}] SSR songs: ${songs.length}`);
-            this.hotHundredSongs = songs.sort((a, b) => a.position - b.position);
-            this.service.updateMainSongPageTitle(`Top ${this.hotHundredSongs.length} Songs of ${this.selectedYear}`);
-          },
-          error: (err) => console.error(`[${yearNum}] SSR fetch error:`, err)
+        // Fetch songs and sort them
+        this.service.getSongs(yearNum, 'position').subscribe((songs) => {
+          this.hotHundredSongs = songs.sort((a, b) => a.position - b.position);
+
+          // Update main song page title (runs on both server and client)
+          this.service.updateMainSongPageTitle(`Top ${this.hotHundredSongs.length} Songs of ${this.selectedYear}`);
         });
       }
     });
